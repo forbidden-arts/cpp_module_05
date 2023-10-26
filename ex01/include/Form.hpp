5ee7f3e657603719c1007d51cf61e093de365ec4
Dan Palmer <dj.palmer81@gmail.com>
2023-10-26 08:42:33 +0300

    ex01

---
+++ ex01/include/Form.hpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmer <dpalmer@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 08:16:23 by dpalmer           #+#    #+#             */
+/*   Updated: 2023/10/26 08:20:26 by dpalmer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <string>
+#include <iostream>
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Bureaucrat;
+
+class Form
+{
+	private:
+		const std::string _name;
+		bool _is_signed;
+		const int _sign_grade;
+		const int _exec_grade;
+
+	public:
+		Form(void);
+		Form(const Form &src);
+		Form(int sign_grade, int exec_grade);
+		Form(const std::string name);
+		Form(const std::string name, int sign_grade, int exec_grade);
+
+		~Form();
+
+		Form &operator=(const Form &src);
+
+		void beSigned(Bureaucrat &signer);
+		
+		const std::string getName(void)const;
+		const std::string getIsSigned(void)const;
+		bool getIsSignedBool(void)const;
+		int getSignGrade(void)const;
+		int getExecGrade(void)const;
+
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+};
+
+std::ostream	&operator<<(std::ostream &o, Form *a);
+
+#endif
